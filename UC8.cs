02b8119c68f9576ee5e360fa286c6c92d25d38f5
shivakar <shivakar@ch.iitr.ac.in>
2021-10-28 13:19:16 +0530

    [shivakar] ADDED: UC8 class added.

---
+++ UC8.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageCalculaton
+{
+    class UC8
+    {
+
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+
+        public static int computeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHourPerMonth)
+        {
+
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+
+            while (totalEmpHrs <= maxHourPerMonth && totalWorkingDays < numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days#:" + totalWorkingDays + "Emp Hrs :" + empHrs);
+
+            }
+            int totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine("Total Emp Wage for company:" + "is :" + totalEmpWage);
+            return totalEmpWage;
+        }
+
+    }
+}
